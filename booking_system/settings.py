4619455739baeed4976cfb971cd1a23b55752967
douglas <douglasmaxton@gmail.com>
2024-01-31 15:45:44 +0000

    add heroku to allow a host list

--- booking_system/settings.py
+++ booking_system/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 # Application definition
 
