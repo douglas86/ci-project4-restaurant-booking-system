26d07eb56034be4000b2ac233f714e06b6c540f9
douglas <douglasmaxton@gmail.com>
2024-02-08 20:06:19 +0000

    added the customer model to the admin panel

--- home/admin.py
+++ home/admin.py
@@ -1,1 +1,10 @@
 # Register your models here.
+from django.contrib import admin
+
+from .models import Customer
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('user_name', 'next_booking', 'table_history', 'role', 'created_at', 'updated_at')
+    search_fields = ('user_name',)
