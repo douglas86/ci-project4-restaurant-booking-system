cc8131a72ddc020dc88a2193992eb1257b6cbc6b
douglas <douglasmaxton@gmail.com>
2024-02-08 20:21:27 +0000

    created the model for chefSpecials

--- home/models.py
+++ home/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+STATUS = ((0, 'Breakfast'), (1, 'Lunch'), (2, 'Supper'))
+
 
 # Create your models here.
 class Customer(models.Model):
@@ -24,3 +26,13 @@ class Customer(models.Model):
 
     class Meta:
         ordering = ['-created_at']
+
+
+class ChefSpecials(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    ingredients = models.JSONField(default=list)
+    served = models.IntegerField(choices=STATUS, default=0)
+    featured_image = CloudinaryField('image', default='placeholder')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
