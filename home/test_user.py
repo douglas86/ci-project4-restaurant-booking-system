bb9e570012577341cbcac52099621bcce5fd4ddb
douglas <douglasmaxton@gmail.com>
2024-02-11 15:31:53 +0000

    added descriptive docstrings to test functions

--- home/test_user.py
+++ home/test_user.py
@@ -2,14 +2,29 @@ from django.test import TestCase, Client
 
 
 class TestViews(TestCase):
+    """
+    Testing if the user can log in or be created
+    """
     def setUp(self):
+        """
+        This method initializes the setup of the user
+        :return:
+        """
         self.client = Client()
         self.create_user = self.client.post('/accounts/signup/', {'username': 'test', 'password': '123456'})
 
     def test_create_user(self):
+        """
+        Run test to see if a user can be created
+        :return:
+        """
         print("Creating new user")
         self.assertEqual(self.create_user.status_code, 200)
 
     def test_login_user(self):
+        """
+        Run test to see if user can be logged in
+        :return:
+        """
         print("Logging in")
         self.assertEqual(self.create_user.status_code, 200)
