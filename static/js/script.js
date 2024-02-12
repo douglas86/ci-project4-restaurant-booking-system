006da3882fc264363682a794028f1b7564a71e72
douglas <douglasmaxton@gmail.com>
2024-02-12 18:31:04 +0000

    hide the auth icon when hamburger menu is open

--- static/js/script.js
+++ static/js/script.js
@@ -2,14 +2,17 @@ const hamburgerMenu = () => {
     let iconLinks = document.getElementById('icon-links')
     let nav = document.getElementById('nav')
     let userMessage = document.getElementById('user-message')
+    let authIcon = document.getElementById('auth-icon')
 
     if (iconLinks.style.display === "block") {
         iconLinks.style.display = "none"
         nav.style.display = "flex"
         userMessage.style.display = "block"
+        authIcon.style.display = "block"
     } else {
         iconLinks.style.display = "block"
         nav.style.display = "block"
         userMessage.style.display = "none"
+        authIcon.style.display = "none"
     }
 }
