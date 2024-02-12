a62c7fd58847f2f35a972ff5bf8d4c073f93e09b
douglas <douglasmaxton@gmail.com>
2024-02-12 22:48:46 +0000

    added icons for phone and email

--- home/views.py
+++ home/views.py
@@ -1,6 +1,14 @@
+import datetime
+
 from django.views.generic import TemplateView
 
 
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = 'home/index.html'
+
+    today = datetime.date.today()
+    year = today.year
+
+    def get_context_data(self, *args, **kwargs):
+        return {'year': self.year}
