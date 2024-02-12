0a4638d233bc12c8aba0b792c2551f37fe3b3b26
douglas <douglasmaxton@gmail.com>
2024-02-12 17:13:40 +0000

    used JavaScript to open and close menu

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,9 @@
-console.log('This is a console')
+const hamburgerMenu = () => {
+    let x = document.getElementById('icon-links')
+
+    if (x.style.display === "block") {
+        x.style.display = "none"
+    } else {
+        x.style.display = "block"
+    }
+}
