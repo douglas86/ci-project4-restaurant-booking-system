92440725f27ef96d1ab53ae4434c7bd10868cf64
douglas <douglasmaxton@gmail.com>
2024-02-21 16:56:37 +0000

    created the method to get the correct meal from specials model

--- menu/views.py
+++ menu/views.py
@@ -1,9 +1,41 @@
+import datetime
 from django.views.generic import TemplateView
+
+from home.models import ChefSpecial
 
 
 # Create your views here.
 class MenuView(TemplateView):
+    """
+    This is the view for the Menu to be displayed
+    """
     template_name = 'menu/menu.html'
+
+    today = datetime.date.today()  # gets current data of laptop
+    current_hour = datetime.datetime.now().strftime(
+        '%H')  # gets the current hour only
+
+    def breakfast_meal(self):
+        return ChefSpecial.objects.filter(served=0)
+
+    def lunch_meal(self):
+        return ChefSpecial.objects.filter(served=1)
+
+    def supper_meal(self):
+        return ChefSpecial.objects.filter(served=2)
+
+    def decide_on_meal(self):
+        if int(self.current_hour) >= 18:
+            return self.supper_meal()  # filters all supper meals
+        elif int(self.current_hour) >= 12:
+            return self.lunch_meal()  # filters all lunch meals
+        elif int(self.current_hour) >= 8:
+            return self.breakfast_meal()  # filters all breakfast meals
+        else:
+            return 3  # returns the default image
+
+    def get_queryset(self):
+        return self.decide_on_meal()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -11,11 +43,11 @@ class MenuView(TemplateView):
         # gets the url path which is a key of slug that was passed
         context['slug'] = self.kwargs['slug']
 
-        print('context', context)
+        print('context', self.get_queryset())
 
         # swi = switch(context)
 
-        return {'meals': meals, 'context': context}
+        return {'meals': meals, 'context': context, 'queryset': self.get_queryset()}
 
 
 def switch(context):
