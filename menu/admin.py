31f8321bac95dbb930c2801e336e50059cb0a97a
douglas <douglasmaxton@gmail.com>
2024-03-02 20:15:51 +0000

    registered the menu model on the admin panel

--- menu/admin.py
+++ menu/admin.py
@@ -1,1 +1,18 @@
 # Register your models here.
+
+from django.contrib import admin
+
+from menu.models import Menu
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    """
+    Class to register the menu on the admin dashboard
+    It will register as Menu and not MenuAdmin
+    """
+
+    # fields that I want to see on the admin panel
+    list_display = ("title", "ingredients", "menu_type", "created_at", "updated_at")
+    # I want to search based on title
+    search_fields = "title"
