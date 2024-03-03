47b41b9d44bd9cf862daf7e4fe1ce87a7dfd09b6
douglas <douglasmaxton@gmail.com>
2024-03-03 15:30:58 +0000

    renamed ingredients to description in menu model

--- menu/models.py
+++ menu/models.py
@@ -11,7 +11,7 @@ class Menu(models.Model):
 
     id = models.AutoField(primary_key=True)
     title = models.CharField(blank=False)
-    ingredients = models.TextField(blank=False)
+    description = models.TextField(blank=False)
     menu_type = models.IntegerField(choices=STATUS, default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
