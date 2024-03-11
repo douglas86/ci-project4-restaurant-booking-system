b3b6e0b758a0f3a62e091d698d8fda7bb60e33fb
douglas <douglasmaxton@gmail.com>
2024-03-11 21:16:08 +0000

    added form input tags with django widgets

--- book_table/views.py
+++ book_table/views.py
@@ -1,7 +1,13 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
+from .form import BookTableForm
+
 
 # Create your views here.
 class BookTable(LoginRequiredMixin, TemplateView):
     template_name = 'book_table/table.html'
+    form_class = BookTableForm
+
+    def get_context_data(self, **kwargs):
+        return {'form': self.form_class()}
