1229d1d196a32057893380773fcb327b65b69f95
douglas <douglasmaxton@gmail.com>
2024-03-15 12:00:29 +0000

    added docstring to my method for forms

--- book_table/form.py
+++ book_table/form.py
@@ -9,12 +9,22 @@ class BookTableForm(forms.ModelForm):
     """
 
     class Meta:
+        """
+        Metaclass is a built-in method to give added functionality to forms
+        """
+
         model = Customer
         fields = ['seats', 'time_slots', ]
         widgets = {'time_slots': forms.widgets.DateTimeInput(attrs={'type': 'datetime-local'})}
         labels = {'seats': 'Number of seats', 'time_slots': 'time slots'}
 
     def __init__(self, *args, **kwargs):
+        """
+        Initializes this class with required fields
+        :param args:
+        :param kwargs:
+        """
+
         super(BookTableForm, self).__init__(*args, **kwargs)
 
         self.fields['seats'].required = True
