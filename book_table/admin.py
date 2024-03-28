757a6ce2c3dc29df66fba80dd7bf1a40b06f5f8c
douglas <douglasmaxton@gmail.com>
2024-03-28 16:37:03 +0000

    deleted: voucher model from database

--- book_table/admin.py
+++ book_table/admin.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 
 from .form import BookTableForm
-from .models import Customer, Voucher
+from .models import Customer
 
 
 @admin.register(Customer)
@@ -13,13 +13,3 @@ class CustomerAdmin(admin.ModelAdmin):
 
     form = BookTableForm
     list_display = ('user', 'seats', 'time_slots', 'created_at', 'updated_at')
-
-
-@admin.register(Voucher)
-class VoucherAdmin(admin.ModelAdmin):
-    """
-    Custom Admin Panel for Vouchers Model
-    """
-
-    list_display = ('title', 'description', 'created_at')
-    search_fields = ('title',)
