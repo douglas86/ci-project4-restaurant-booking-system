007f7a1b393fb22ad9329c0a7cdc77a7e8c6d92b
douglas <douglasmaxton@gmail.com>
2024-04-01 17:51:56 +0100

    changed: recognises timezones in settings.py

--- booking_system/settings.py
+++ booking_system/settings.py
@@ -137,9 +137,9 @@ ACCOUNT_EMAIL_VERIFICATION = "none"
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "en-gb"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/London"
 
 USE_I18N = True
 
