70d835e433303f48d36ace0b2d8adf7fba2f56fe
douglas <douglasmaxton@gmail.com>
2024-07-17 15:25:25 +0100

    fix: redirects to correct password change template

--- booking_system/forms.py
+++ booking_system/forms.py
@@ -21,3 +21,31 @@ class UsernameResetPasswordForm(forms.Form):
         # email = User.objects.get(username=username).email
         # self.cleaned_data['email'] = email
         return temporary_password
+
+
+class ChangePasswordForm(forms.Form):
+    username = forms.CharField(label='Username', max_length=150)
+    password = forms.CharField(label='Password', max_length=150)
+    old_password = forms.CharField(label='Old Password', max_length=150)
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+
+        if not User.objects.filter(username=username).exists():
+            raise forms.ValidationError('There is no user registered with this username.')
+        return username
+
+    def clean_old_password(self):
+        old_password = self.cleaned_data['old_password']
+
+        if not User.objects.filter(username=old_password).exists():
+            raise forms.ValidationError('There is no old password registered with this username.')
+        return old_password
+
+    def save(self, request, **kwargs):
+        username = self.cleaned_data['username']
+        password = self.cleaned_data['password']
+        user = User.objects.get(username=username)
+        user.set_password(password)
+        user.save()
+        return user
--- booking_system/views.py
+++ booking_system/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 
-from .forms import UsernameResetPasswordForm
+from .forms import UsernameResetPasswordForm, ChangePasswordForm
 
 
 class UsernamePasswordResetView(View):
@@ -18,3 +18,18 @@ class UsernamePasswordResetView(View):
             temporary_password = form.save(request)
             return render(request, 'account/password_reset_done.html', {'temporary_password': temporary_password})
         return render(request, self.template_name, {'form': form})
+
+
+class ChangePasswordView(View):
+    form_class = ChangePasswordForm
+    template_name = 'account/password_change.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            new_password = form.save(request)
+            return render(request, 'account/login.html', {'new_password': new_password})
